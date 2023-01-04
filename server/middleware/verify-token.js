65f04a2fca7f222666f8ce4389ca09e3568300c3
jenshire <chocotoast2001@gmail.com>
2023-01-05 05:49:38 +0900

    다시 수정

--- server/middleware/verify-token.js
+++ server/middleware/verify-token.js
@@ -4,6 +4,9 @@ const secret_key = require('../utils/secret_key.json');
 const HttpError = require('../models/http-error');
 
 module.exports = (req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        return next();
+    }
     try {
         const token = req.headers.authorization.split(' ')[1];
         if (!token) {
