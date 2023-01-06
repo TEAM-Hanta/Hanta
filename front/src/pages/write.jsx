417f93b21e2ae03b9599f095ad59e3cf239bebe8
lee-ou-jin <77gkswns@naver.com>
2023-01-06 19:30:23 +0900

    index 수정본

--- front/src/pages/write.jsx
+++ front/src/pages/write.jsx
@@ -1,42 +1,55 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { Component } from 'react';
+import React,{ useEffect,useState } from "react";
 import "../css/write.css"
 
-class Write extends Component {
-  constructor(props) {
-    super(props)
-  }
+function Write() {
+  const [todoList, setTodoList] = useState([]);
 
-  render() {
+  useEffect(() => {
+      fetch('https://localhost:8080/add')
+      .then((response) => response.json())
+      .then((data) => setTodoList(data))
+      .catch(rejected => {
+        console.log(rejected);
+      });
+  },  []);
 
+  const onSubmitHandler = (e) => {
+      const text = e.target.text.value;
+      const done = e.target.done.value;
+      fetch('https://localhost:8080/add', {
+          method : 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              text,
+              done,
+
+          }),
+      });
+  };
     return (
         <>
+         <form onSubmit={onSubmitHandler}>
         <div style={{paddingLeft:"15%"}}>
         <h1><FontAwesomeIcon icon="arrow-left"/>
+        
         <button className="button">올리기</button>
         </h1>
-        </div>
-        
-
-          
-          
+        </div>       
         <div className='Write'>
             <div>
-        <input type='text' id='title_txt' placeholder='제목'/>
+        <input type='text' id='title_txt' placeholder='제목' name="text"/>
             </div>
         
             <div>
-        <textarea id='content_txt' placeholder='글을 작성해보세요.'></textarea>
+        <textarea id='content_txt' placeholder='글을 작성해보세요.' name="done"/>
             </div>
         </div>
-          
-
-           
+         </form>  
         </>
     )
   }
-}
 
 export default Write;
-
-
