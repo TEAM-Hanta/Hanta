f362c814fcd6d462092066308d5facd9ee6365e3
jenshire <chocotoast2001@gmail.com>
2023-01-13 23:01:20 +0900

    자격증 관련 코드 추가

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 const postRoutes = require('./routes/post-routes');
 const userRoutes = require('./routes/user-routes');
+const otherRoutes = require('./routes/other-routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api', userRoutes);
+app.use('/api', otherRoutes);
 app.use('/api/posts', postRoutes);
 
 app.get('/', function (요청, 응답) {
@@ -33,6 +35,5 @@ app.use((error, req, res, next) => {
     res.status(error.code || 500);
     res.json({ message: error.message || '알 수 없는 오류가 발생했습니다.' });
 });
-
 
 app.listen(8080);
