fa3579ea9cfcea6ef6d9592a368f97ad1fdf0c46
lee-ou-jin <77gkswns@naver.com>
2023-02-11 16:40:13 +0900

    병합충돌제거

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -6,29 +5,15 @@ import "./App.css";
 import Home from "./pages/home/home"; //홈페이지
 import Board from "./pages/home/boardlist"; //개별게시판
 import Detail from "./pages/home/detailpost"; //상세 게시물
-=======
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-//홈페이지 이하
-import Home from './pages/home/home'; //홈페이지
-import Board from './pages/home/boardlist'; //개별게시판
-import Detail from './pages/home/detailpost'; //상세 게시물
->>>>>>> 43fa50b2d355ed77e9136679ccaa8da3599978b7
 //자격증 이하
-import License from './pages/license/license'; //자격증
+import License from "./pages/license/license"; //자격증
 //마이페이지 이하
-<<<<<<< HEAD
 import Mypage from "./pages/mypage/mypage"; //마이페이지
-=======
-import Mypage from './pages/mypage/mypage'; //마이페이지
->>>>>>> 43fa50b2d355ed77e9136679ccaa8da3599978b7
 //알림페이지 이하
-import Notice from './pages/notice/notice'; //알리페이지
+import Notice from "./pages/notice/notice"; //알리페이지
 //글쓰기페이지 이하
-import Write from './pages/write/write'; //글쓰기
+import Write from "./pages/write/write"; //글쓰기
 //유저 페이지 이하
-<<<<<<< HEAD
 import Login from "./user/login"; //로그인
 import Signup from "./user/signup"; //회원가입
 
@@ -60,8 +45,7 @@ const App = () => {
 	return (
 		<div className="App">
 				{token ? (
-					<>  
-						<BottomNav />
+					<>
 						<Routes>
 							<Route path="/home" element={<Home />} />
 							<Route path="/boardlist" element={<Board />} />
@@ -74,7 +58,7 @@ const App = () => {
 							<Route path="/detail/:id" element={<Detail />} />
 							<Route path="/*" element={<div style={{ fontSize: "50px", textAlign: "center" }}>잘못된 접근입니다.</div>} />
 						</Routes>
-						
+						<BottomNav />
 
 					</>
 				) : (
@@ -84,34 +68,6 @@ const App = () => {
 				)}
 		</div>
 	);
-=======
-import Login from './user/login'; //로그인
-import Signup from './user/signup'; //회원가입
-import Note from './pages/note/note';
-import DetailNote from './pages/home/detailNote';
-import WriteNote from './pages/write/writeNote';
-
-const App = () => {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/boardlist" element={<Board />} />
-                <Route path="/" element={<Login />} /> {/* 추후 home주소로 등록시키고 미 로그인시 접근할때 로그인창이 뜨도록 하기 */}
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/license" element={<License />} />
-                <Route path="/notice" element={<Notice />} />
-                <Route path="/mypage" element={<Mypage />} />
-                <Route path="/mynote" element={<Note />} /> {/* 쪽지함 */}
-                <Route path="/note" element={<WriteNote />} /> {/* 쪽지작성 */}
-                <Route path="/note/:id" element={<DetailNote />} /> {/* 쪽지내용 보기 */}
-                <Route path="/write" element={<Write />} />
-                <Route path="/detail/:id" element={<Detail />} />
-                <Route path="/*" element={<div style={{ fontSize: '50px', textAlign: 'center' }}>잘못된 접근입니다.</div>} />
-            </Routes>
-        </div>
-    );
->>>>>>> 43fa50b2d355ed77e9136679ccaa8da3599978b7
 };
 
 export default App;
--- front/src/user/login.jsx
+++ front/src/user/login.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React , { useState , useEffect  } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import "./login.css"
@@ -72,68 +72,6 @@ function Login({setToken}) {
     </div>
   )
 }
-=======
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import ErrorMessage from '../pages/home/Components/error';
-import './login.css';
-
-function Login() {
-    const [error, setError] = useState([]);
-
-    const onSubmitHandler = async (e) => {
-        e.preventDefault();
-        const id = e.target.id.value;
-        const password = e.target.password.value;
-
-        try {
-            const response = await fetch('http://localhost:8080/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    id,
-                    password,
-                }),
-            });
-            const data = await response.json();
-
-            if (!response.ok) {
-                throw new Error(data.message);
-            }
-            console.log('로그인 성공');
-        } catch (err) {
-            setError(err.message || '알 수 없는 에러가 발생했습니다.');
-        }
-    };
-    return (
-        <div className="loginy">
-            <img style={{ width: '300px', height: '300px', paddingBottom: '20px' }} alt="bg" src="img/hanta-Logo.png" />
-            <form onSubmit={onSubmitHandler}>
-                <input className="input" style={{ fontSize: '22px' }} description="id" placeholder=" 아이디" type="text" name="id" />
-                <br />
-                <input className="input" style={{ fontSize: '22px' }} description="Password" placeholder=" 비밀번호" type="password" name="password" />
-                {error.length !== 0 ? <ErrorMessage error={error} /> : null}
-                <OtherMethods />
-            </form>
-        </div>
-    );
-}
-
-const OtherMethods = (props) => (
-    <div className="alternativeLogin">
-        <label>
-            <Link to="/signUp" className="link">
-                회원이 아니신가요?
-            </Link>
-        </label>
-        <br />
-        <button className="sibar">로그인</button>
-    </div>
-);
->>>>>>> 43fa50b2d355ed77e9136679ccaa8da3599978b7
-
 export default Login;
 
 // css추가 부가기능추가
