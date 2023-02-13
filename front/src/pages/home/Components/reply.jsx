d247e659c53f39a8c28323a5a5c62b4da42fec7c
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2023-02-13 20:06:07 +0900

    handleReplyClick 함수 currying
    작성된 handleReplyClick은 함수이므로 handleReplyClick(index) 이런 형태로 쓸 수 있을 것이다.
    이 때 handleReplyClick(index)은 "() => setShowReply(...)" 이 함수를 호출하는 함수이다.
    그 결과, 형태를 변경할 수 없는 함수(특히 콜백함수)의 실행 전에 특정 변수(여기선 index)를 매개변수로 제공하여 다음에 호출할 함수에서 사용할 수 있다.
    TMI로 이런 형태의 함수는 "handleReplyClick(index)( )" 이렇게 함수 호출을 의미하는 ( )를 두 번 사용할 수 있다.
    이 짓거리를 전문 용어로 currying이라 함.

--- front/src/pages/home/Components/reply.jsx
+++ front/src/pages/home/Components/reply.jsx
@@ -1,71 +1,72 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React,{useState} from "react";
-import "./reply.css"
+import React, { useState } from "react";
+import "./reply.css";
 
 function Reply(props) {
-    const [showReply, setShowReply] = useState(props.value.map(() => false));
-  
-    const handleReplyClick = (index) => {
-        setShowReply(prevState => {
-          const newState = [...prevState];
-          newState[index] = !newState[index];
-          return newState;
-        });
-    };
-    
-    const onSubmitHandler = async (e) => {
-        e.preventDefault();
+	const [showReply, setShowReply] = useState(props.value.map(() => false));
 
-        // const content = e.target.content.value;
-        // const post_type = e.target.post_type.value; //라디오 버튼으로 게시판 추가하기
-        // // const anonymous = isChecked;
-        // const userId = localStorage.getItem("userId");
+	const handleReplyClick = (index) => () =>
+		setShowReply((prevState) => {
+			const newState = [...prevState];
+			newState[index] = !newState[index];
+			return newState;
+		});
 
+	const onSubmitHandler = async (e) => {
+		e.preventDefault();
 
-        try {
-            const response = await fetch('http://localhost:8080/api/posts/reply2', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    // content,
-                    // userId,
-                    // post_type,
-                    // anonymous,
-                }),
-            });
-            const data = await response.json();
-            // navigate('/');
+		// const content = e.target.content.value;
+		// const post_type = e.target.post_type.value; //라디오 버튼으로 게시판 추가하기
+		// // const anonymous = isChecked;
+		// const userId = localStorage.getItem("userId");
 
-            if (!response.ok) {
-                throw new Error(data.message);
-            }
-        } catch (err) {
-            // setError(err.message || '알 수 없는 에러가 발생했습니다.');
-        }
-    };
-    console.log(props.value);
-    return (
-        <>
-        <div>
+		try {
+			const response = await fetch("http://localhost:8080/api/posts/reply2", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					// content,
+					// userId,
+					// post_type,
+					// anonymous,
+				}),
+			});
+			const data = await response.json();
+			// navigate('/');
 
-            {props.value?.map((reply) => (
-                <div key={reply.index}>
-                    <br></br>
-                    <div className= {reply.layer ?  'reply' : 'reply_reply'}>
-                    <h4>프로필 : {reply.anonymous === 1? '익명':reply.nickname}</h4>
-                    <h4>{reply.content}</h4>
-                    <a>{new Date(reply.created_at).toLocaleString("ko-KR",{timeZone:'UTC'})}</a>
-                    <button onClick={handleReplyClick(reply.index)}>답글</button>
-                    {showReply[reply.index] && <form><input type="text" /><button type="submit">작성</button></form>}
-                    </div>
-                </div>
-            ))}
-
-        </div>
-        </>
-    );
+			if (!response.ok) {
+				throw new Error(data.message);
+			}
+		} catch (err) {
+			// setError(err.message || '알 수 없는 에러가 발생했습니다.');
+		}
+	};
+	console.log(props.value);
+	return (
+		<>
+			<div>
+				{props.value?.map((reply, idx) => (
+					<div key={idx}>
+						<br></br>
+						<div className={reply.layer ? "reply" : "reply_reply"}>
+							<h4>프로필 : {reply.anonymous === 1 ? "익명" : reply.nickname}</h4>
+							<h4>{reply.content}</h4>
+							<a>{new Date(reply.created_at).toLocaleString("ko-KR", { timeZone: "UTC" })}</a>
+							<button onClick={handleReplyClick(idx)}>답글</button>
+							{showReply[idx] && (
+								<form>
+									<input type="text" />
+									<button type="submit">작성</button>
+								</form>
+							)}
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
 }
 
 export default Reply;
