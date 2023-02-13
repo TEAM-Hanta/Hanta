7718a8bfe5e32c1a068a64cc2fdfc3a4dde6b60e
lee-ou-jin <77gkswns@naver.com>
2023-02-13 14:36:27 +0900

    검색결과없음 추가

--- front/src/pages/home/Components/list.jsx
+++ front/src/pages/home/Components/list.jsx
@@ -6,48 +6,56 @@ import { Link } from "react-router-dom";
 
 function List({value}) {
     const data = value.read();
+    console.log(data);
     return (
         <>
-        {data. map( v =>
-        <div key={ v .id}>
-            <div className="list">
-
-            <Link style={{textDecoration:"none", color:"black"}} to={'/detail/' + v.id} >
+        { data.length ? 
+        <>    
+            {data. map( v =>
+                <div key={ v .id}>
+                <div className="list">
+                
+                <Link style={{textDecoration:"none", color:"black"}} to={'/detail/' + v.id} >
                 <div> 
-                    <br/>
-                    <h4 style={{fontSize:"30px", fontWeight:"bold"}}><FontAwesomeIcon icon="user" />{v.anonymous === 1 ? '익명' : v.nickname}</h4>
-
+                <br/>
+                <h4 style={{fontSize:"30px", fontWeight:"bold"}}><FontAwesomeIcon icon="user" />{v.anonymous === 1 ? '익명' : v.nickname}</h4>
+                
                 </div>
-
-
-            <div>
-                    <div style={{fontSize:"20px"}}> 제목: {v.title}
-                    <div style={{color:"gray", float:"right", fontSize:"13px", paddingTop:"13px"}}>{new Date(v.created_at).toLocaleString("ko-KR",{timeZone:'UTC'})}</div>
-                    </div>
-                    
-                    <div>
-                    <div style={{fontSize:"20px"}}> 내용: {v.content.substr(0,5)}... {/* 내용 축약 시켜서 내보내기 */}
-                        <div className="sibal">
-                            <Count value ={{likes : v.like_count,reply : v.reply_count}}/>
-                        </div>
-                    </div>
-                        
-                        
-                    </div>
-                    
-
-                    
-                    
-                    
+                
+                
+                <div>
+                <div style={{fontSize:"20px"}}> 제목: {v.title}
+                <div style={{color:"gray", float:"right", fontSize:"13px", paddingTop:"13px"}}>{new Date(v.created_at).toLocaleString("ko-KR",{timeZone:'UTC'})}</div>
+                </div>
+                
+                <div>
+                <div style={{fontSize:"20px"}}> 내용: {v.content.substr(0,5)}... {/* 내용 축약 시켜서 내보내기 */}
+                <div className="sibal">
+                <Count value ={{likes : v.like_count,reply : v.reply_count}}/>
+                </div>
+                </div>
+                
+                
+                </div>
+                
+                
+                
+                
+                
                     
             </div>
-                    
-                   
+            
+            
             </Link>
+            
+            </div>
+            </div>
+            )}
+            </>:
 
-            </div>
-            </div>
-        )}
+        <div>검색 결과가 없습니다</div>
+        } 
+        
         </>
     );
 }
