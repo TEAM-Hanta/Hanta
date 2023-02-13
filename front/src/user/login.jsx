3e35df5e0eeb77c0eff759f570e6ffd822c2dee7
김지연 <102957732+jenshire@users.noreply.github.com>
2023-02-13 22:39:24 +0900

    에러 처리

--- front/src/user/login.jsx
+++ front/src/user/login.jsx
@@ -1,77 +1,70 @@
-
-import React , { useState , useEffect  } from "react";
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import "./login.css"
+import './login.css';
 import { useRecoilState } from 'recoil';
 import { tokenState } from '../token/GlobalState';
+import ErrorMessage from '../pages/home/Components/error';
 
+function Login({ setToken }) {
+    const navigate = useNavigate();
+    const [error, setError] = useState([]);
+    // const [tokenData, setTokenData] = useState('');
+    // const [token, setToken] = useRecoilState(tokenState);
 
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const id = e.target.id.value;
+            const password = e.target.password.value;
+            const res = await fetch('http://localhost:8080/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id,
+                    password,
+                }),
+            });
+            const resData = await res.json();
+            if (!res.ok) {
+                throw new Error(resData.message);
+            }
 
-function Login({setToken}) {
-  const navigate = useNavigate();
+            localStorage.setItem('token', resData.token);
+            localStorage.setItem('userId', resData.userId);
+            setToken(resData.token);
+            navigate('/');
+        } catch (err) {
+            setError(err.message);
+        }
+    };
 
-  // const [tokenData, setTokenData] = useState('');
-  // const [token, setToken] = useRecoilState(tokenState);
-
-  
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    try{
-      const id = e.target.id.value;
-      const password = e.target.password.value;
-      fetch('http://localhost:8080/api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id,
-          password,
-        }),
-      })
-        .then((res) => {
-          if (res.ok) {
-            return res.json();
-          }
-          throw new Error('Failed to fetch');
-        })
-        .then((v) => {
-          localStorage.setItem("token", v.token);
-          localStorage.setItem("userId", v.userId);
-          setToken(v.token);
-          navigate('/');
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-
-
-
-    const OtherMethods = props => (
-      <div className="alternativeLogin">
-        <label><Link to="/signUp" className="link">회원이 아니신가요?</Link></label>
-        <br />
-        <button className="sibar">로그인</button>
-      </div>
+    const OtherMethods = (props) => (
+        <div className="alternativeLogin">
+            <label>
+                <Link to="/signUp" className="link">
+                    회원이 아니신가요?
+                </Link>
+            </label>
+            <br />
+            <button className="sibar">로그인</button>
+        </div>
     );
-    
 
-  return (
-    <div className="loginy">
-      <img style={{ width: "300px", height: "300px", paddingBottom: "20px" }} alt="bg" src="img/hanta-Logo.png" />
-      <form onSubmit={onSubmitHandler}>
-        <input className="input" style={{ fontSize: "22px" }} description="id" placeholder=" 아이디" type="text" name="id" />
-        <br />
-        <input className="input" style={{ fontSize: "22px" }} description="Password" placeholder=" 비밀번호" type="password" name="password" />
-        <OtherMethods />
-      </form>
-    </div>
-  )
+    return (
+        <div className="loginy">
+            <img style={{ width: '300px', height: '300px', paddingBottom: '20px' }} alt="bg" src="img/hanta-Logo.png" />
+            <form onSubmit={onSubmitHandler}>
+                <input className="input" style={{ fontSize: '22px' }} description="id" placeholder=" 아이디" type="text" name="id" />
+                <br />
+                <input className="input" style={{ fontSize: '22px' }} description="Password" placeholder=" 비밀번호" type="password" name="password" />
+                {error.length !== 0 ? <ErrorMessage error={error} /> : null}
+
+                <OtherMethods />
+            </form>
+        </div>
+    );
 }
 export default Login;
 
